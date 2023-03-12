97719ca69cc3209e524cdc62ab3ad3afce6d0f5e
dborione <db1997@outlook.fr>
2023-03-12 15:51:11 +0100

    another sort five

--- sort_small.c
+++ sort_small.c
@@ -47,41 +47,130 @@ int ft_sort_three(t_queue *queue)
 //     return (1);
 // }
 
+// int ft_sort_five(t_queue *queue_a, t_queue *queue_b)
+// {
+//     ft_push(queue_a, queue_b);
+//     ft_push(queue_a, queue_b);
+//     ft_sort_three(queue_a);
+//     // if (queue_b->head->value < queue_b->tail->value)
+//     //     ft_swap(queue_b);
+//     if (queue_b->head->value < queue_a->head->value)
+//         ft_push(queue_b, queue_a);
+//     if (queue_b->head->value < queue_a->head->value)
+//     {
+//             ft_push(queue_b, queue_a);
+//         if (queue_a->head->value > queue_a->head->next->value)
+//             ft_swap(queue_a);
+//         return (1);
+//     }
+
+//     while (queue_b->head->value < queue_a->tail->value
+//         && queue_b->head->value > queue_a->head->value)
+//         ft_rr_reverse_rotate(queue_a);
+//     while (queue_b->head->value < queue_a->tail->value)
+//         ft_rr_reverse_rotate(queue_a);
+//     ft_push(queue_b, queue_a);
+//     if (queue_a->head->value < queue_a->tail->value
+//         && queue_a->head->value < queue_a->head->next->value)
+//     {
+//         ft_rr_reverse_rotate(queue_a);
+//         ft_swap(queue_a);
+//     }
+//     while (queue_a->head->value > queue_a->tail->value)
+//         ft_r_rotate(queue_a);
+//     while (queue_a->head->value < queue_b->head->value)
+//         ft_r_rotate(queue_a);
+//     ft_push(queue_b, queue_a);
+//     while (queue_a->tail->value < queue_a->head->value)
+//         ft_rr_reverse_rotate(queue_a);
+//     return (1);
+// }
+
+int ft_get_min(t_queue *queue)
+{
+    int min;
+    t_node	*tmp;
+
+    min = queue->head->value;
+	tmp = queue->head;
+	while(tmp)
+	{
+        if (tmp->value <= min)
+            min = tmp->value;
+		tmp = tmp->next;
+	}
+	free(tmp);
+	return (min);
+}
+
+int ft_get_max(t_queue *queue)
+{
+    int max;
+    t_node	*tmp;
+
+    max = queue->head->value;
+	tmp = queue->head;
+	while(tmp)
+	{
+        if (tmp->value >= max)
+            max = tmp->value;
+		tmp = tmp->next;
+	}
+	free(tmp);
+	return (max);
+}
 int ft_sort_five(t_queue *queue_a, t_queue *queue_b)
 {
-    ft_push(queue_a, queue_b);
-    ft_push(queue_a, queue_b);
-    ft_sort_three(queue_a);
-    // if (queue_b->head->value < queue_b->tail->value)
-    //     ft_swap(queue_b);
-    if (queue_b->head->value < queue_a->head->value)
-        ft_push(queue_b, queue_a);
-    if (queue_b->head->value < queue_a->head->value)
-    {
-            ft_push(queue_b, queue_a);
-        if (queue_a->head->value > queue_a->head->next->value)
-            ft_swap(queue_a);
-        return (1);
-    }
+    int min;
+    int max;
+    int i;
 
-    while (queue_b->head->value < queue_a->tail->value
-        && queue_b->head->value > queue_a->head->value)
-        ft_rr_reverse_rotate(queue_a);
-    while (queue_b->head->value < queue_a->tail->value)
-        ft_rr_reverse_rotate(queue_a);
-    ft_push(queue_b, queue_a);
-    if (queue_a->head->value < queue_a->tail->value
-        && queue_a->head->value < queue_a->head->next->value)
+    i = 2;
+    min = ft_get_min(queue_a);
+    max = ft_get_max(queue_a);
+   // if (queue_a->head->value != min)
+
+    while (i > 0)
     {
-        ft_rr_reverse_rotate(queue_a);
-        ft_swap(queue_a);
+        /* sort queue_a like: mid - max - min */
+        if (queue_a->head->value != min && queue_a->head->value != max )
+        {
+            ft_r_rotate(queue_a);
+            i--;
+        }
+        if (queue_a->head->value == min)
+        {
+            ft_r_rotate(queue_a);
+            i--;
+        }
+        if (queue_a->head->value == max && queue_a->tail->value == min)
+        {
+            ft_swap(queue_a);
+            i--;
+        }
+        if (queue_a->head->value == max && queue_a->tail->value != min)
+        {
+            ft_rr_reverse_rotate(queue_a);
+            i--;
+        }
+
+        while (queue_a->head->value != min 
+            && queue_a->head->value != max)
+            ft_push(queue_a, queue_b);
     }
-    while (queue_a->head->value > queue_a->tail->value)
-        ft_r_rotate(queue_a);
-    while (queue_a->head->value < queue_b->head->value)
-        ft_r_rotate(queue_a);
+    if (queue_a->head->value == max && queue_a->tail->value == min)
+        ft_swap(queue_a);
+
+    if (queue_b->head->value < queue_b->tail->value)
+        ft_swap(queue_b);
+    
     ft_push(queue_b, queue_a);
-    while (queue_a->tail->value < queue_a->head->value)
-        ft_rr_reverse_rotate(queue_a);
+    if (queue_a->head->value > queue_a->head->next->value)
+        ft_swap(queue_a);
+    ft_push(queue_b, queue_a);
+    if (queue_a->head->value > queue_a->head->next->value)
+        ft_swap(queue_a);
+    
+    ft_rr_reverse_rotate(queue_a);
     return (1);
 }
