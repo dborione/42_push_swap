7bba6757ff1b167010dab1319ab87c70d4fec192
Donatienne Borione <db1997@outlook.fr>
2023-04-12 11:56:09 +0200

    fix double

--- src/parsing.c
+++ src/parsing.c
@@ -50,11 +50,79 @@ int  ft_double_check(t_queue *queue)
     // return (1);
 
     t_node *tmp;
+    t_node *tmp2;
     int j;
     int i;
+    // int x;
 
+    // tmp = queue->head;
+    // tmp2 = queue->head;
+    // i = 1;
+    // j = ft_queue_size(queue);
+    // //x = tmp->value;
+    // while (i != j)
+    // {
+    //     while (j > 1)
+    //     {
+    //         x = tmp->value;
+    //         printf("%d:", x);
+    //         if (tmp == queue->tail)
+    //             j--;
+    //         else
+    //         {
+    //             tmp = tmp->next;
+    //             x = tmp->value;
+    //             printf("%d\n", tmp2->value);
+    //             if (tmp2->value == x)
+    //                 return (0);
+    //             j--;
+    //         }
+    //     }
+    //     if (tmp2 == queue->tail)
+    //         i++;
+    //     else
+    //     {
+    //         tmp2 = tmp2->next;
+    //         j = ft_queue_size(queue) - i;
+    //         i++;
+    //     }
+    // }
+    // return (1);
 
-
+    tmp = queue->head;
+    tmp2 = tmp->next;
+    i = ft_queue_size(queue);
+    j = 0;
+    while (j < ft_queue_size(queue))
+    {
+        while (i > 1)
+        {
+            printf("%d:", tmp->value);
+            printf("%d\n", tmp2->value); 
+            if (tmp->value == tmp2->value)
+                return (0);
+            if (tmp2 == queue->tail)
+                i--;
+            else
+            {
+                tmp2 = tmp2->next;
+                i--;
+            }
+        }
+        printf("j = %d, ", j);
+        printf("i = %d\n", i);
+        if (tmp == queue->tail)
+            j++;
+        else
+        {
+            tmp = tmp->next;
+            tmp2 = tmp;
+            tmp2 = tmp2->next;
+            j++;
+            i = ft_queue_size(queue) - j;
+        }
+    }
+    return (1);
 }
 
 /*
