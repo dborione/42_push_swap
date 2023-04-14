67f1ef8de0a2969081f80072225478b98f27463d
Donatienne Borione <db1997@outlook.fr>
2023-04-14 13:45:11 +0200

    all clean

--- src/ft_atoi.c
+++ src/ft_atoi.c
@@ -6,24 +6,18 @@
 /*   By: dborione <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 13:18:29 by dborione          #+#    #+#             */
-/*   Updated: 2023/04/14 13:20:21 by dborione         ###   ########.fr       */
+/*   Updated: 2023/04/14 13:42:58 by dborione         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-static int	ft_atoi_utils(char *str, int i, int op, long nbr, t_queue *queue)
+static	int	ft_atoi_utils(char *str, int i)
 {
-	if (str[i] == '\0')
-		ft_exit(queue, 1);
-	while (str[i] >= '0' && str[i] <= '9')
-		nbr = nbr * 10 + (str[i++] - '0');
-	if (str[i] != '\0')
-		ft_exit(queue, 1);
-	nbr = nbr * op;
-	if (nbr > INT_MAX || nbr < INT_MIN)
-		ft_exit(queue, 1);
-	return (nbr);
+	while (str[i] == ' ' || str[i] == '\f' || str[i] == '\n' || str[i] == '\r'
+		|| str[i] == '\t' || str[i] == '\v')
+		i++;
+	return (i);
 }
 
 int	ft_atoi(char *str, t_queue *queue)
@@ -33,21 +27,24 @@ int	ft_atoi(char *str, t_queue *queue)
 	int		count;
 	long	nbr;
 
-	i = 0;
 	op = 1;
 	nbr = 0;
 	count = 0;
-	while (str[i] == ' ' || str[i] == '\f' || str[i] == '\n' || str[i] == '\r'
-		|| str[i] == '\t' || str[i] == '\v')
-		i++;
+	i = ft_atoi_utils(str, 0);
 	while (str[i] == '+' || str[i] == '-')
 	{
-		if (str[i] == '-')
+		if (str[i++] == '-')
 			op = -1;
-		i++;
 		count++;
 	}
-	if (count > 1)
+	if (count > 1 || str[i] == '\0')
 		ft_exit(queue, 1);
-	return (ft_atoi_utils(str, i, op, nbr, queue));
+	while (str[i] >= '0' && str[i] <= '9')
+		nbr = nbr * 10 + (str[i++] - '0');
+	if (str[i])
+		ft_exit(queue, 1);
+	nbr = nbr * op;
+	if (nbr > INT_MAX || nbr < INT_MIN)
+		ft_exit(queue, 1);
+	return (nbr);
 }
